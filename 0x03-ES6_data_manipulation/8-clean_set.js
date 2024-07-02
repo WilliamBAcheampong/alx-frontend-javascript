558d290d5bfa6b9cd523d6faaf8ef8ae3cae02ea
unknown <boachieawilliam@gmail.com>
2024-07-02 23:27:15 +0000

    updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (startString.length > 0) {
-    const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-    const cleanedValues = filteredValues.map((value) => value.substring(startString.length));
-    return cleanedValues.join('-');
-  }
-  return '';
-export default function cleanSet(set, startString) {
   const list = [];
 
   if (
